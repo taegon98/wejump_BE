0df00a4bf7eb6c873ed7eebc12f5f9125ec5401f
taegon1998 <taegon1998@gmail.com>
2023-10-30 23:47:14 +0900

    update: OAuth redirect Url 수정

--- server/src/main/java/wejump/server/api/controller/member/OAuthController.java
+++ server/src/main/java/wejump/server/api/controller/member/OAuthController.java
@@ -1,7 +1,6 @@
 package wejump.server.api.controller.member;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -13,35 +12,51 @@ import org.springframework.web.bind.annotation.RestController;
 import wejump.server.api.dto.member.MemberResponseDTO;
 import wejump.server.domain.member.Member;
 import wejump.server.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Map;
 
 @RestController
 @RequestMapping("/oauth")
 @RequiredArgsConstructor
 @CrossOrigin
+@Slf4j
 public class OAuthController {
 
     private final MemberRepository memberRepository;
+
+    private static final String DEFAULT_ROLE = "Student";
+
     @GetMapping("/loginInfo")
-    public ResponseEntity<MemberResponseDTO> oauthLoginInfo(Authentication authentication){
-        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-        String email = attributes.get("email").toString();
+    public ResponseEntity<MemberResponseDTO> oauthLoginInfo(Authentication authentication) {
+        try {
+            OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+            Map<String, Object> attributes = oAuth2User.getAttributes();
+            String email = attributes.get("email").toString();
 
-        Member member = new Member();
+            // 검색된 회원 정보 저장
+            Member member;
+            if (memberRepository.findByEmail(email).isPresent()) {
+                member = memberRepository.findByEmail(email).get();
+            } else {
+                member = Member.builder()
+                        .name(attributes.get("name").toString())
+                        .email(email)
+                        .role(DEFAULT_ROLE)
+                        .build();
+                memberRepository.save(member);
+            }
 
-        if (memberRepository.findByEmail(email).isEmpty()) {
-            Member new_member = Member.builder()
-                    .name(attributes.get("name").toString())
-                    .email(email)
-                    .role("Student")
-                    .build();
-            memberRepository.save(new_member);
+            // 리다이렉션 응답 생성
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Location", "http://localhost:5173/weJump_Fe/"); // 외부 URL로 절대 경로로 설정
+
+            return new ResponseEntity<>(headers, HttpStatus.SEE_OTHER); // 303 See Other 리다이렉션 상태 코드
+
+        } catch (Exception e) {
+            log.error("OAuth login info error: " + e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        else member = memberRepository.findByEmail(email).get();
-
-        MemberResponseDTO memberResponseDTO = new MemberResponseDTO(member.getId(), member.getName(), member.getEmail(), member.getRole());
-
-        return new ResponseEntity<>(memberResponseDTO, HttpStatus.OK);
     }
 }
