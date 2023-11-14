5ea6d0e67813f2217c2a613745cdf5bbb79d646a
taegon1998 <taegon1998@gmail.com>
2023-11-14 12:35:10 +0900

    refact/Oauth-cookie 반환 수정

--- server/src/main/java/wejump/server/api/controller/member/OAuthController.java
+++ server/src/main/java/wejump/server/api/controller/member/OAuthController.java
@@ -1,5 +1,6 @@
 package wejump.server.api.controller.member;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,11 +10,15 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.CookieGenerator;
 import wejump.server.api.dto.member.MemberResponseDTO;
 import wejump.server.domain.member.Member;
 import wejump.server.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Base64;
 import java.util.Map;
 
 @RestController
@@ -30,7 +35,10 @@ public class OAuthController {
     private static final String FRONTEND_REDIRECT_URL = "http://localhost:5173/weJump_Fe/"; // 프론트엔드 리다이렉션 URI
 
     @GetMapping("/loginInfo")
-    public ResponseEntity<MemberResponseDTO> oauthLoginInfo(Authentication authentication) {
+    public ResponseEntity<MemberResponseDTO> oauthLoginInfo(
+            Authentication authentication,
+            HttpServletResponse response
+    ) {
         try {
             OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
             Map<String, Object> attributes = oAuth2User.getAttributes();
@@ -41,7 +49,7 @@ public class OAuthController {
             if (memberRepository.findByEmail(email).isPresent()) {
                 member = memberRepository.findByEmail(email).get();
             } else {
-                // 새로운 회원 생성
+                // Create a new member
                 member = Member.builder()
                         .name(attributes.get("name").toString())
                         .email(email)
@@ -50,14 +58,24 @@ public class OAuthController {
                 memberRepository.save(member);
             }
 
-            // 멤버 정보를 MemberResponseDTO로 매핑
+            // Member information to MemberResponseDTO
             MemberResponseDTO memberDTO = new MemberResponseDTO(
                     member.getId(), member.getName(), member.getEmail(), member.getRole());
 
-            // 프론트엔드 리다이렉션 URI로 리디렉트
+            // MemberResponseDTO to JSON and then encode as Base64
+            ObjectMapper objectMapper = new ObjectMapper();
+            String memberDTOJson = objectMapper.writeValueAsString(memberDTO);
+            String base64EncodedMemberDTO = Base64.getEncoder().encodeToString(memberDTOJson.getBytes());
+
+            // Store the Base64 encoded data in the cookie
+            CookieGenerator cookieGenerator = new CookieGenerator();
+            cookieGenerator.setCookieName("MEMBERDTO"); // Set your desired cookie name
+            cookieGenerator.addCookie(response, base64EncodedMemberDTO);
+
+            // Frontend redirection URI
             HttpHeaders headers = new HttpHeaders();
             headers.add("Location", FRONTEND_REDIRECT_URL);
-            return new ResponseEntity<>(memberDTO, headers, HttpStatus.SEE_OTHER); // 303 See Other 리다이렉션 상태 코드
+            return new ResponseEntity<>(memberDTO, headers, HttpStatus.SEE_OTHER);
         } catch (Exception e) {
             log.error("OAuth login info error: " + e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
